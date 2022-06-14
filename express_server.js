6da015c9673623421f21ac9857b5354b53c08847
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-13 20:20:57 -0600

    Random genorator and urls/new

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 
@@ -8,6 +9,45 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let rString = ""
+  let i = 0
+  while (i < 6){
+    if (Math.random() > 0.5) {
+      if (Math.random() > 0.5) {
+        rString + String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+      } else {
+        rString + String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+      }
+    } else {
+      rString + Math.floor(Math.random() * 10);
+    }
+    i ++;
+  }
+  return rString
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
