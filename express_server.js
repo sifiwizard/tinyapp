6479ac8cf7bc23d38fafe166f0b48149707e3e14
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-13 20:41:18 -0600

    Redirects and geting

--- express_server.js
+++ express_server.js
@@ -16,16 +16,16 @@ function generateRandomString() {
   while (i < 6){
     if (Math.random() > 0.5) {
       if (Math.random() > 0.5) {
-        rString + String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        rString += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
       } else {
-        rString + String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+        rString += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
       }
     } else {
-      rString + Math.floor(Math.random() * 10);
+      rString += Math.floor(Math.random() * 10);
     }
     i ++;
   }
-  return rString
+  return rString;
 }
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -39,14 +39,21 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/", (req, res) => {
