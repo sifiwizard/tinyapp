460338f7be2be004209e929596adec7fbdabef65
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-14 20:20:53 -0600

    Cookies

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+let cookieParser = require('cookie-parser');
+
 
 app.set("view engine", "ejs");
 
@@ -10,10 +12,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
-  let rString = ""
-  let i = 0
-  while (i < 6){
+const generateRandomString = function() {
+  let rString = "";
+  let i = 0;
+  while (i < 6) {
     if (Math.random() > 0.5) {
       if (Math.random() > 0.5) {
         rString += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -26,27 +28,31 @@ function generateRandomString() {
     i ++;
   }
   return rString;
-}
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(cookieParser());
+
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL
+  const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -78,6 +84,16 @@ app.post("/urls/:id", (req, res) => {
   const longURL = req.body.longURL;
   const id = req.params.id;
   urlDatabase[id] = longURL;
+  res.redirect("/urls");
+});
+
+app.post("/login" , (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
